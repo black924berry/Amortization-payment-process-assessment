f7e2d3fbcc05f766dca668972de991a25f4f6e45
blackberry <angelfirst124@outlook.com>
2023-06-08 20:23:24 +0300

    Vue frontend

---
+++ Vue.js Amortization List/amortization-list-test/src/pages/HomePage/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <ul class="nav">
+      <li>
+        <router-link to="/video-games" exact class="nav-link">VIDEO GAMES</router-link>
+      </li>
+      <li>
+        <router-link to="/contact" exact class="nav-link">CONTACT</router-link>
+      </li>
+    </ul>
+    <div class="main">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'HomePage',
+  methods: {
+    ...mapActions(['getGameList'])
+  },
+  created() {
+    this.getGameList()
+  }
+}
+</script>
+<style scoped>
+@import '@/assets/css/home.css';
+</style>
